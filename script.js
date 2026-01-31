451e0b7fbaaed0b012eee07e2a74e734b779a6b2
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-02-01 00:44:46 +0530

    complete 10 question

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ console.log(a);
 console.error("this is error");
 
 
+let square=12
+console.log(square*square);
+
+
+if(1)console.log(true);
+
+// let age =Number(prompt("Enter your age "))
+// if(age>=18)console.log(age);
+// else{
+//     console.log("you age is less than 18");
+    
+// }
+
+console.log(100/0);
+
+
+// ## 2. Variables and Data Types (10 Questions)
+
