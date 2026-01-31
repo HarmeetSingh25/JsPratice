c97990e9e73ea48f7297cb426fccc346dbed7180
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-01-31 23:50:24 +0530

    Solve 6 question

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log(`My name is Harmeet and my hobby is Do coding`);
+
+console.log((45*2)-10);
+
+let year= new Date()
+console.log(year.getFullYear());
+
+let firstname ="Harmeet"
+let lastname="singh"
+console.log(firstname+" " +lastname);
+
+var a= 10
+console.log(a);
+var a=15
+console.log(a);
+
+console.error("this is error");
+
+
